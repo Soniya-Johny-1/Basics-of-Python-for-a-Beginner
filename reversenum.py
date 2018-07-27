86bbe5fbbc6e8a8d7b94d11609c1af6fbb0f01f2
Soniya Johny <41745378+Soniya-Johny-1@users.noreply.github.com>
2018-07-27 21:31:40 +0530

    Create reversenum.py
    Python program to reverse number

---
+++ reversenum.py
@@ -0,0 +1,15 @@
+# Python Program to Reverse a Number using While loop
+ 
+Number = int(input("Please Enter any Number: "))
+Reverse = 0
+while(Number > 0):
+    Reminder = Number %10
+    Reverse = (Reverse *10) + Reminder
+    Number = Number //10
+ 
+print("\n Reverse of entered number is = %d" %Reverse)
+
+#output
+# Please Enter any Number:432
+#  Reverse of entered number is =234
+
